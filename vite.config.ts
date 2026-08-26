d0f7e29a33c7ade5d4a46926e363671a96f13030
tpham0322 <truongpham1173@gmail.com>
2026-08-26 11:30:29 -0400

    Task Filter implementation with vitest pass

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tailwindcss(),
   ],
-})
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+});
